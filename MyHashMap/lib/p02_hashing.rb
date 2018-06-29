f5d4039edd4e455de4642d0abf3db39689f0a7d5
zamandalee <a@zalee.me>
2018-06-29 16:19:52 -0700

    Complete po3 and po4 linked list exercises

--- MyHashMap/lib/p02_hashing.rb
+++ MyHashMap/lib/p02_hashing.rb
@@ -6,6 +6,7 @@ class Array
   def hash
     alphabet = ('a'..'z').to_a
     sum = 0
+    
     each_with_index do |el, idx|
       sum += el * idx if el.is_a?(Integer)
       sum += alphabet.find_index(el.downcase) * idx if el.is_a?(String)
--- MyHashMap/lib/p04_linked_list.rb
+++ MyHashMap/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Node
   attr_accessor :key, :val, :next, :prev
 
@@ -15,11 +17,17 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @prev.next, @next.prev = @next, @prev
   end
 end
 
 class LinkedList
+  include Enumerable
+  
   def initialize
+    @head = Node.new("head")
+    @tail = Node.new("tail")
+    @head.next, @tail.prev = @tail, @head
   end
 
   def [](i)
@@ -28,34 +36,69 @@ class LinkedList
   end
 
   def first
+    @head.next unless empty?
   end
 
   def last
+    @tail.prev unless empty?
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    return nil unless include?(key)
+
+    each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    each do |node|
+      return true if node.key == key
+    end
+    
+    false
   end
 
   def append(key, val)
+    new_node = Node.new(key, val)
+    new_node.prev = @tail.prev
+    new_node.next = @tail
+    
+    @tail.prev.next = new_node
+    @tail.prev = new_node
   end
+  
 
   def update(key, val)
+    return nil if empty?
+    
+    each do |node|
+      node.val = val if node.key == key
+    end
   end
 
   def remove(key)
+    each do |node|
+      node.remove if node.key == key
+    end
   end
 
-  def each
+  def each(&prc)
+    current_node = first
+    
+    until current_node == @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
