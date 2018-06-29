3663defe9fa3a84e84183650cd96eb5a6ec2a0f7
zamandalee <a@zalee.me>
2018-06-28 17:32:36 -0700

    Complete two_sum bad and okay methods

--- two_sum.rb
+++ two_sum.rb
@@ -1,3 +1,42 @@
-def bad_two_sum?(arr, target)
+def bad_two_sum?(arr, target) #O(n^2) time complexity
+  (0...arr.length - 1).each do |idx|
+    (idx + 1...arr.length).each do |jdx|
+      return true if arr[idx] + arr[jdx] == target
+    end
+  end
+  
+  false
+end
+
+def okay_two_sum?(arr, target)
+  arr = arr.sort  #[1,2,3,3,4] target = 7
+  
+  arr.each_with_index do |num, i|
+    new_target = target - num
+    idx = arr[i + 1..-1].b_search(new_target)
+    return true unless idx.nil?
+  end 
+  
+  false
+end 
+
+class Array
+  
+  def b_search(target, &prc)
+    prc ||= Proc.new {|a,b| a <=> b}
+    
+    return nil if length == 0
+    middle = length / 2
+    
+    case prc.call(self[middle], target)
+    when 0
+      return middle
+    when 1
+      return self.dup.take(middle).b_search(target, &prc)
+    else
+      search_res = self.dup.drop(middle + 1).b_search(target, &prc)
+      search_res.nil? ? nil : middle + 1 + search_res
+    end
+  end
   
 end
