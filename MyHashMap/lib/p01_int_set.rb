a6d168d87c29972c96dc264f5e657a5f0f16898b
zamandalee <a@zalee.me>
2018-06-29 14:32:35 -0700

    Complete p01 and p02 hashing exercises

---
+++ MyHashMap/lib/p01_int_set.rb
@@ -0,0 +1,118 @@
+class MaxIntSet
+  attr_reader :max, :store
+  
+  def initialize(max)
+    @max = max
+    @store = Array.new(max)
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num.between?(0, max-1)
+  end
+
+  def validate!(num)
+    raise "Out of bounds" unless is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num unless include?(num)
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    resize! if @count == num_buckets
+    unless include?(num)
+      self[num] << num
+      @count += 1
+    end
+  end
+
+  def remove(num)
+    temp = []
+    if include?(num)
+      @store[num].each do |el|
+        temp << el unless el == num
+      end
+      @count -= 1
+      @store[num] = temp
+    end
+    
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    new_buckets = num_buckets * 2
+      
+    temp = Array.new(new_buckets) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |num|
+        temp[num % new_buckets] << num
+      end
+    end  
+    @store = temp
+  end
+end
---
+++ MyHashMap/lib/p02_hashing.rb
@@ -0,0 +1,37 @@
+class Fixnum
+  # Fixnum#hash already implemented for you
+end
+
+class Array
+  def hash
+    alphabet = ('a'..'z').to_a
+    sum = 0
+    each_with_index do |el, idx|
+      sum += el * idx if el.is_a?(Integer)
+      sum += alphabet.find_index(el.downcase) * idx if el.is_a?(String)
+    end
+    
+    sum.hash
+  end
+end
+
+class String
+  def hash
+    alphabet = ('a'..'z').to_a
+    sum = 0
+    
+    chars.each_with_index do |letter, idx|
+      sum += alphabet.find_index(letter.downcase) * idx
+    end
+    
+    sum.hash
+  end
+end
+
+class Hash
+  # This returns 0 because rspec will break if it returns nil
+  # Make sure to implement an actual Hash#hash method
+  def hash
+    keys.sort.map(&:to_s).hash
+  end
+end
