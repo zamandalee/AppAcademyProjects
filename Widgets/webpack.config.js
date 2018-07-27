fe209fb26bbefacacad0632723046e7f920fc0cc
zamandalee <a@zalee.me>
2018-07-27 14:48:44 -0700

    Complete Clock Widget functions and styling

--- Widgets/webpack.config.js
+++ Widgets/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './frontend/entry.js',
+  entry: './frontend/widgets.jsx',
   output: {
     path: path.resolve(__dirname),
     filename: 'bundle.js'
