073df2bac68dc37a6243ac298fba740792e087bf
zamandalee <a@zalee.me>
2018-07-27 17:36:52 -0700

    Complete Tabs Widget function

--- Widgets/frontend/widgets.jsx
+++ Widgets/frontend/widgets.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './clock';
+import Tabs from './tabs';
+
+function generateTabs() {
+  return [
+    {title: "tab1", content: "I love ponies"},
+    {title: "tab2", content: "I love dorks"},
+    {title: "tab3", content: "I love red"}
+  ];
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Clock />, root);
+  ReactDOM.render(
+    <div>
+      <Clock />
+      <Tabs tabs={generateTabs()} />
+    </div>,
+  root);
 });
