4c584a55b9ade2714338b439988004605f2bb833
zamandalee <a@zalee.me>
2018-07-30 14:35:07 -0700

    Complete todos app redux cycle(phase2)

---
+++ frontend/components/content.jsx
@@ -0,0 +1,8 @@
+// import React from 'react';
+// import { Provider } from 'react-redux';
+//
+// export const content = ({ store }) => (
+//   <Provider store={store}>
+//     <App />
+//   </Provider>
+// );
--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import { receiveTodos, receiveTodo } from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   const content = document.getElementById('content');
+  const store = configureStore();
+  window.store = store;
+  window.receiveTodos = receiveTodos;
+  window.receiveTodo = receiveTodo;
   ReactDOM.render(<h1>Todos App</h1>, content);
 });
