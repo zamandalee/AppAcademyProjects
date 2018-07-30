bd43f7cc244f119912964c82e5563d8d7e55afe9
zamandalee <a@zalee.me>
2018-07-30 11:53:40 -0700

    Complete todos app phase 1:FrontEndStructure

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const content = document.getElementById('content');
+  ReactDOM.render(<h1>Todos App</h1>, content);
+});
