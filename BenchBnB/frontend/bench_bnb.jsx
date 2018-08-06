a2279639db3d0525fcd00bbfe8555c67b7622b35
zamandalee <a@zalee.me>
2018-08-06 10:45:12 -0700

    Complete BenchBnB Phase1: Front-End User Auth

---
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createUser, createSession, deleteSession } from './util/session_api_util';
+import { configureStore } from './store/store';
+
+document.addEventListener( 'DOMContentLoaded', () => {
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
+  const root = document.getElementById('root');
+  // window.deleteSession = deleteSession;
+
+  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+});
