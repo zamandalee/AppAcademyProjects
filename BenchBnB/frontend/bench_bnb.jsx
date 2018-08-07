74358f5423118daf9f44958fdbed2e9960e7ef71
zamandalee <a@zalee.me>
2018-08-06 23:29:48 -0700

    Complete BenchBnb Phase2: React Router and Session Components up to Greetig components

--- BenchBnB/frontend/bench_bnb.jsx
+++ BenchBnB/frontend/bench_bnb.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createUser, createSession, deleteSession } from './util/session_api_util';
 import { configureStore } from './store/store';
+import Root from './components/root';
 
 document.addEventListener( 'DOMContentLoaded', () => {
   const store = configureStore();
@@ -9,7 +9,5 @@ document.addEventListener( 'DOMContentLoaded', () => {
   window.dispatch = store.dispatch;
 
   const root = document.getElementById('root');
-  // window.deleteSession = deleteSession;
-
-  ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
